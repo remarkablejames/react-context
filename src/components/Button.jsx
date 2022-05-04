6dfa59180a5976d4438aecccdb717f50fa378e2c
remarkablejames <lascasasjames85@gmail.com>
2022-05-04 10:04:50 +0200

    wired up react context to the application

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,11 @@
 import React, { Component } from "react";
+import LanguageContext from "../contexts/LanguageContext";
 
 class Button extends Component {
-  state = {};
+  static contextType = LanguageContext;
   render() {
-    return <button className="ui button primary">Submit</button>;
+    const text = this.context === "english" ? "Submit" : "Voorleggen";
+    return <button className="ui button primary">{text}</button>;
   }
 }
 
--- src/components/Field.jsx
+++ src/components/Field.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
+import LanguageContext from "../contexts/LanguageContext";
 class Field extends Component {
-  state = {};
+  static contextType = LanguageContext;
   render() {
+    const text = this.context === "english" ? "Name" : "Naam";
     return (
       <div className="ui field">
-        <label>Name</label>
+        <label>{text}</label>
         <input type="text" />
       </div>
     );
